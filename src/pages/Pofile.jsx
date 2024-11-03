58ded3c890f5a48c8745390cb04b1c781b6e0174
investor-wallflex <mustaphasharafa70@gmail.com>
2024-11-03 12:22:21 +0100

    new modifications

--- src/pages/Pofile.jsx
+++ src/pages/Pofile.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Profile.css';
 import profileImage from '../assets/kl.png'; 
+import { MdVerified } from "react-icons/md";
+import { FaBirthdayCake } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+import { FaCode } from "react-icons/fa";
 
 const Profile = () => {
   return (
     <div className="profile">
       <div className="profile-header">
-        <div className="cover-photo">
-         
-        </div>
+        <div className="cover-photo"></div>
         <div className="profile-photo">
           <img src={profileImage} alt="Profile" />
         </div>
-        <h1>Sharafa Wale Mustapha <span className="emoji">💋</span></h1>
-        <p>Brings ideas to life with code! ✨</p>
+        <h1>Sharafa Wale Mustapha <span className="emoji"><MdVerified />
+        </span></h1>
+        <p>Brings ideas to life with code! <FaCode /></p>
         <p className="bio">
             Entry-level developer with a knack for creativity <br />and a strong foundation in coding principles. <br /> Committed to continuous learning and improving  <br />my skills in front-end.
         </p>
@@ -22,12 +25,18 @@ const Profile = () => {
       <div className="profile-links">
         <span className="icon">🔗</span>
         <a href="/links">/links</a>
-        <span className="icon">📅</span>
-        <span>Novermber 9th</span>
-        <span className="icon">📅</span>
+        <span className="icon"><FaBirthdayCake /></span>
+        <span>November 9th</span>
+        <span className="icon"><SlCalender /></span>
         <span>Joined Nov 2024</span>
       </div>
       <p className="footer-text">Stay updated on my latest projects and adventures!</p>
+
+      <form className="form-container">
+        <input type="email" placeholder="Email Address" />
+        <button type="submit">Subscribe!</button>
+      </form>
+      <p className="note">No spam. Unsubscribe any time.</p>
     </div>
   );
 };
